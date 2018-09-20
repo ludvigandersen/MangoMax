e339d163884085df526279b2a9f6df11eeeed6cf
DESKTOP-EBQ3QD0\mega- <ludvig.andersen@yahoo.dk>
2018-09-20 14:43:46 +0200

    Initial commit

---
+++ src/main/java/mangomax/demo/MangoMaxApplication.java
@@ -0,0 +1,12 @@
+package mangomax.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangoMaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangoMaxApplication.class, args);
+	}
+}
