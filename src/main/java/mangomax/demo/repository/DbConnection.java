cbf5aa4a4c93684c863bc0ee378ba43f0b186386
Sidney Schultz <tewzdk@gmail.com>
2018-09-25 11:19:14 +0200

    createUser repository

--- src/main/java/mangomax/demo/repository/DbConnection.java
+++ src/main/java/mangomax/demo/repository/DbConnection.java
@@ -39,6 +39,18 @@ public class DbConnection implements IDbRepository {
     @Override
     public void createUser(User user, int roleId) {
 
+
+        jdbc.update("INSERT INTO mangomax.user (user_id, user_name,user_email," +
+                        "user_phone,user_password,userRole_fk) " +
+                        "VALUES (?,?,?,?,?,?)",
+                user.getUserId(),
+                user.getUserName(),
+                user.getUserMail(),
+                user.getUserPhoneNumber(),
+                user.getUserPassword(),
+                user.getFkRoleId());
+
+
     }
 
     @Override
